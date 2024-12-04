22bb0dcaa3ca032aac113bc6d33b256130878f3d
gibbyDev <118143161+gibbyDev@users.noreply.github.com>
2024-12-04 12:40:30 -0500

    Update userModel.go

--- models/userModel.go
+++ models/userModel.go
@@ -1,35 +1,9 @@
-// package models
-
-// import (
-// 	"gorm.io/gorm"
-// )
-
-// type User struct {
-// 	gorm.Model
-// 	Email    string `json:"email" gorm:"unique;not null"`
-// 	Password string 
-// 	Name     string `json:"name"`
-// 	Role     string `json:"role"`
-// }
-
-
-
 package models
 
 import (
     "time"
 	"gorm.io/gorm"
 )
-
-// type User struct {
-// 	gorm.Model
-// 	Email     string `json:"email" gorm:"unique"`
-// 	Password  string `json:"password"`
-// 	Name      string `json:"name"`
-// 	Role      string `json:"role"`
-// 	Active    bool   `json:"active" gorm:"default:false"`
-// 	VerificationToken string `json:"-" gorm:"unique"`
-// } 
 
 type User struct {
     gorm.Model
